527273edc78d3454cddbf33c7a42ab3ba3fc51cd
lucasgcb <lucascoliado@hotmail.com>
2021-03-29 22:15:53 -0300

    Add deployment configuration files

---
+++ jungle_code/django-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+echo "Running migrations!"
+python manage.py makemigrations jungle_api
+python manage.py migrate
+echo "Recreating django super user now..."
+echo "from django.contrib.auth.models import User; import os; User.objects.filter(username='admin').delete(); User.objects.create_superuser(os.environ['WEB_ADMIN_NAME'], os.environ['WEB_ADMIN_EMAIL'], os.environ['WEB_ADMIN_PW'])" | python manage.py shell
+
+if [ ! -d "./jungle_code/static-django" ]; then
+	echo "Deploying static django files..."
+	python manage.py collectstatic --noinput
+fi
+
+if [ $PYSVR = "1" ]; then
+	python manage.py runserver 0.0.0.0:8000 
+else
+	gunicorn --bind=0.0.0.0 jungle_code.wsgi
+fi
