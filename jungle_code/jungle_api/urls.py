086688ee5fe933d9a6bb0637f859fa867b22bd71
lucasgcb <lucascoliado@hotmail.com>
2021-03-31 09:30:39 -0300

    Add description to Redocs schema

--- jungle_code/jungle_api/urls.py
+++ jungle_code/jungle_api/urls.py
@@ -12,7 +12,7 @@ schema_view = get_schema_view(
    openapi.Info(
       title="Snippets API",
       default_version='v1',
-      description="Test description",
+      description="Jungle API for most of your article serving needs",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
