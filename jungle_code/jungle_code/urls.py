5d2ced7db52daf15767c0035c2ef436bbb29541e
lucasgcb <lucascoliado@hotmail.com>
2021-03-31 00:32:04 -0300

    Add route to api app

--- jungle_code/jungle_code/urls.py
+++ jungle_code/jungle_code/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url('admin/', admin.site.urls),
+    url('api/', include('jungle_api.urls')),
 ]
