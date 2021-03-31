7a79d08edb457be85d836afaabf49102a3e35467
lucasgcb <lucascoliado@hotmail.com>
2021-03-31 00:22:13 -0300

    Add auth migration, read super user env variable

--- jungle_code/django-entrypoint.sh
+++ jungle_code/django-entrypoint.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 echo "Running migrations!"
 python manage.py makemigrations jungle_api
+python manage.py migrate auth
 python manage.py migrate
 echo "Recreating django super user now..."
-echo "from django.contrib.auth.models import User; import os; User.objects.filter(username='admin').delete(); User.objects.create_superuser(os.environ['WEB_ADMIN_NAME'], os.environ['WEB_ADMIN_EMAIL'], os.environ['WEB_ADMIN_PW'])" | python manage.py shell
+echo "from django.contrib.auth.models import User; import os; User.objects.filter(username=os.environ['WEB_ADMIN_NAME']).delete(); User.objects.create_superuser(os.environ['WEB_ADMIN_NAME'], os.environ['WEB_ADMIN_EMAIL'], os.environ['WEB_ADMIN_PW'])" | python manage.py shell
 
 if [ ! -d "./jungle_code/static-django" ]; then
 	echo "Deploying static django files..."
